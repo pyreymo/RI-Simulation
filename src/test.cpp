bbe77f443c448f5253484f121e01fce544d5d853
Relic <Relic@DESKTOP-MK4S5PQ>
2022-03-17 15:18:56 +0800

    MPI并行支持测试

--- src/test.cpp
+++ src/test.cpp
@@ -1,7 +1,17 @@
+#include "mpi.h"
 #include "Simulation.h"
 
-int main() {
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int rank, size;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
 	Simulation s(2, 8, 0.7);
 
-	return -1;
+    MPI_Finalize();
 }
